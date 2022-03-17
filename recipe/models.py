06c3944fbcc6b4ffee7178dc6dfcb690b315def9
Sharpryan20 <sharpryan01@icloud.com>
2022-03-17 14:05:17 +0000

    created class Comment in models.py

--- recipe/models.py
+++ recipe/models.py
@@ -27,4 +27,19 @@ class Post(models.Model):
 
     def number_of_likes(self):
         return self.likes.count()
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=120)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=True)
 
+    class Meta:
+        """
+        Show order of comments.
+        """
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
