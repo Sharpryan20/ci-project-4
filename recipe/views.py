75d48980e02274704d088c94a77a7a74ee9dd253
Sharpryan20 <sharpryan01@icloud.com>
2022-03-22 11:01:07 +0000

    fixed bug so post cards actually show

--- recipe/views.py
+++ recipe/views.py
@@ -5,9 +5,9 @@ from .models import Post
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    queryset = Post.objects.all().order_by('-created_on')
     template_name = 'index.html'
-    paginate_by = 10
+    paginate_by = 6
 
 class PostDetail(View):
 
